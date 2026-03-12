49470bfdaafa89bd4c794921a8a81e4265a428c7
tidclibya2026 <tidc2026@gmail.com>
2026-03-12 11:06:55 +0000

    Update map.js

--- js/map.js
+++ js/map.js
@@ -1,8 +1,43 @@
+// إنشاء الخريطة
 var map = L.map('map').setView([26.3351,17.2283],6);
 
+// طبقة الخريطة
 L.tileLayer(
 'https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png',
 {
 maxZoom:18
+}).addTo(map);
+
+
+// طبقات البيانات
+var hotelLayer = L.layerGroup().addTo(map);
+var restaurantLayer = L.layerGroup().addTo(map);
+var cafeLayer = L.layerGroup().addTo(map);
+
+
+// تحميل الفنادق
+fetch("data/hotels.geojson")
+.then(response=>response.json())
+.then(data=>{
+
+L.geoJSON(data,{
+
+pointToLayer:function(feature,latlng){
+
+return L.marker(latlng);
+
+},
+
+onEachFeature:function(feature,layer){
+
+layer.bindPopup(
+"<b>"+feature.properties.name+"</b><br>"
++
+feature.properties.city
+);
+
 }
-).addTo(map);
+
+}).addTo(hotelLayer);
+
+});
